34c407736e776b9b839122a4ef9e0d3b78c2897a
Marian Lyzhychka <mr.lezhichka@gmail.com>
2021-03-24 15:30:47 +0200

    test

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from '../../assets/styles/app.scss'
+
+const App = () => {
+    return (
+        <div>
+            Hello world
+        </div>
+    );
+};
+
+export default App;
